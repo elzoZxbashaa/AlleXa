a33da0cbde121db1df18f0f307ca5808c9df2c6c
elzoZxbashaa <ELZOZXBASHAA@gmail.com>
2024-06-21 00:38:17 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -6,14 +6,14 @@ from pyrogram import filters
 
 load_dotenv()
 
-OWNER = ["R7_OX"]
+OWNER = ["V_K_Z"]
 OWNER_NAME = " 𝗥͜𝗼͡𝗪͡𝗲͜𝗦 "
 BOT_TOKEN = getenv("BOT_TOKEN")
 DATABASE = getenv("MONGO_DB_URI", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-CHANNEL = "https://t.me/RQ_SF"
-GROUP = "https://t.me/R3_QX"
-VID_SO = "https://telegra.ph/file/bf1273e084a0fb135ab5a.jpg"
-PHOTO = "https://telegra.ph/file/bf1273e084a0fb135ab5a.jpg"
-LOGS = "jabababbab"
+CHANNEL = "https://t.me/zxxlz"
+GROUP = "https://t.me/qkpqp"
+VID_SO = "https://graph.org/file/510a7702f5400271266fa.jpg"
+PHOTO ="https://graph.org/file/510a7702f5400271266fa.jpg"
+LOGS = "elzoz"
